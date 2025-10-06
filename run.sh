09b279b485c578039208629045e0198811f3dd15
stxch <enzostach68@gmail.com>
2025-10-06 18:46:29 -0300

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-# Faz o script parar se algum comando falhar
-set -e
+set -e # Faz o script parar se algum comando falhar
 
 # --- ETAPA 1: PREPARAÇÃO DO AMBIENTE ---
 echo "==> Etapa 1: Instalando dependências..."
+# A coleção é instalada automaticamente pelo Semaphore se o collections/requirements.yml existir
+# ansible-galaxy collection install -r collections/requirements.yml
 pip install -r requirements.txt
 
 # --- ETAPA 2: EXECUÇÃO DA AUTOMAÇÃO ---
 echo "==> Etapa 2: Executando o playbook Ansible..."
 
-# O comando ansible-playbook agora passa as variáveis do formulário para o playbook principal
+# A linha abaixo foi corrigida para usar variáveis de ambiente ($)
 ansible-playbook \
   -i inventory \
-  --extra-vars "vm_count={{.Quantidade_de_VMs_para_criar}} vm_name_prefix='{{.Prefixo_para_o_nome_das_VMs}}' template_name='{{.Nome_do_template_a_ser_clonado}}'" \
+  --extra-vars "vm_count=$vm_count vm_name_prefix='$vm_name_prefix' template_name='$template_name'" \
   deploy.yml
 
 echo "==> FIM DA EXECUÇÃO <=="
